6099ca594e61ae6f7f9af2838144aaf0ee07db36
Andre <AndreAndreM@proton.me>
2024-08-21 14:38:16 +0200

    add LoadDefaultSession and SaveDefaultSessionAndQuit to main module exports

--- lua/kiss-sessions/init.lua
+++ lua/kiss-sessions/init.lua
@@ -3,5 +3,7 @@ local core = require("kiss-sessions.core")
 return {
     SaveSession = core.SaveSession,
     LoadSession = core.LoadSession,
+    LoadDefatulSession = core.LoadDefatulSession,
+    SaveDefaultSessionAndQuit = core.SaveDefaultSessionAndQuit,
     setup = core.setup,
 }
